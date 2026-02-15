4ef77d72fcdfe71a3413ee61363c66eba2cafedd
snowflakeworld <snow.miller.frozen@outlook.com>
2026-02-15 15:32:28 -0500

    fix: implement new functions

--- digitalmarket.sol
+++ digitalmarket.sol
@@ -2,19 +2,12 @@
 pragma solidity ^0.8.26;
 
 contract DigitalMarket {
-    uint256 private _productIdCounter;
-    address public platformOwner;
-    uint256 public platformFee = 500; // default 5%
-
-    uint256 public constant FEE_DENOMINATOR = 10000; // Divisor
-    uint256 public constant MAX_FEE = 1000; // maxfee 10%
-
-    bool public constant IS_TEST = true;
-
-    constructor() {
-        platformOwner = msg.sender;
-    }
-
+    /*
+     *   Type Declarations
+     *   struct Product
+     *   constant uint256 FEE_DENOMINATOR, MAX_PLATFORM_FEE, MAX_LISTING_FEE
+     *   constant bool _IS_TEST
+     */
     struct Product {
         uint256 id;
         string name;
@@ -26,14 +19,34 @@ contract DigitalMarket {
         uint256 createdAt;
     }
 
-    mapping(uint256 => Product) public products;
-    mapping(address => uint256[]) public userProducts;
+    uint256 public constant FEE_DENOMINATOR = 10000; // Fee Divisor (100%)
+    uint256 public constant MAX_PLATFORM_FEE = 1000; // Platform Maxfee 10%
+    uint256 public constant MAX_LISTING_FEE = 1000; // Product Listing maxfee 10%
+
+    bool private constant _IS_TEST = true;
 
     /*
-     *  EVENTS
-     *  ProductListed, ProductPurchased, OwnershipTransferred, PlatformFeeUpdated, FundsWithdrawn
-     *
+     *   State Variables
+     *   private address _platformOwner, _productIdCounter
+     *   public uint256 productIdCounter, platformFee, listingFee
+     *   public mapping products, user2products
      */
+
+    address private _platformOwner;
+    uint256 private _productIdCounter;
+
+    uint256 public platformFee = 500; // MarketPlace Platform Fee -> default 5%
+    uint256 public listingFee = 500; // Product Listing Fee -> default 5%
+
+    mapping(uint256 => Product) public products;
+    mapping(address => mapping(uint256 => bool)) public user2products;
+
+    /*
+     *   Events
+     *   ProductListed, ProductPurchased, OwnershipTransferred, FundsWithdrawn, ProductStatusChanged
+     *   PlatformOwnerChanged, PlatformFeeUpdated, ListingFeeUpdated
+     */
+
     event ProductListed(
         uint256 indexed productId,
         string name,
@@ -56,24 +69,29 @@ contract DigitalMarket {
         address indexed newOwner
     );
 
-    event ProductAvailabilityChanged(
+    event FundsWithdrawn(address indexed owner, uint256 amount);
+
+    event ProductStatusChanged(
         address indexed owner,
         uint256 indexed productId,
         bool enabled
     );
 
+    event PlatformOwnerChanged(address oldOwner, address newOwner);
+
     event PlatformFeeUpdated(uint256 oldFee, uint256 newFee);
 
-    event FundsWithdrawn(address indexed owner, uint256 amount);
+    event ListingFeeUpdated(uint256 oldFee, uint256 newFee);
 
     /*
      *  MODIFIERS
-     *  productExists, onlyProductOwner
+     *  productExisting, onlyProductOwner, onlyPlatformOwner, sufficientBalance
      *
      */
 
     modifier productExisting(uint256 productId) {
-        require(productId < _productIdCounter, "Product does not exist.");
+        require(productId > 0, "Invalid Product Id");
+        require(productId <= _productIdCounter, "Product does not exist");
         _;
     }
 
@@ -84,33 +102,41 @@ contract DigitalMarket {
 
     modifier onlyPlatformOwner() {
         require(
-            platformOwner == msg.sender,
-            "Only the platform owner can do this."
+            _platformOwner == msg.sender,
+            "Only the platform owner can do this"
         );
         _;
     }
 
-    // FUNCTIONS
+    modifier validPrice(uint256 price) {
+        require(price > 0, "Invalid price");
+        _;
+    }
+
+    modifier sufficientBalance(uint256 price) {
+        require(msg.sender.balance >= price, "Unsufficient balance");
+        _;
+    }
 
     /*
-     * Remote product from user products(Internal)
-     * @params (address user, uint256 productId)
-     * @returns(void)
+     *  Functions
+     *  listProduct, purchaseProduct, transferOwnership, withdrawFunds
+     *  updatePlatformOwner, updatePlatformFee, updateListingFee
+     *  updateProductStatus, getProduct, isUserOwningPRoduct, getTotalProductCount
+     *  calculatePlatformFeePrice, calculateListingFeePrice
      */
-    function remoteFromUserProducts(
-        address owner,
-        uint256 productId
-    ) internal productExisting(productId) {
-        for (uint256 i = 0; i < userProducts[owner].length; ++i) {
-            if (userProducts[owner][i] == productId) {
-                userProducts[owner][i] = userProducts[owner][
-                    userProducts[owner].length - 1
-                ];
-                userProducts[owner].pop();
-                break;
-            }
-        }
+
+    constructor(uint256 _platformFee, uint256 _listingFee) {
+        _platformOwner = msg.sender;
+
+        platformFee = _platformFee;
+        listingFee = _listingFee;
     }
+
+    // FallBacks
+    receive() external payable {}
+    fallback() external payable {}
+
 
     /*
      * list Product
@@ -122,10 +148,15 @@ contract DigitalMarket {
         string calldata description,
         uint256 price,
         string calldata ipfsHash
-    ) external returns (uint256) {
+    ) external payable validPrice(price) sufficientBalance(price) returns (uint256) {
         require(bytes(name).length > 0, "Name cannot be empty");
-        require(price > 0, "Price must be greater than 0");
         require(bytes(ipfsHash).length > 0, "IPFS hash requiredd");
+        require(
+            msg.value >= calculateListingFeePrice(price),
+            "Insufficient pay amount for listing a product"
+        );
+
+        // List a product
 
         uint256 productId = ++_productIdCounter;
 
@@ -135,18 +166,18 @@ contract DigitalMarket {
             description: description,
             price: price,
             ipfsHash: ipfsHash,
-            owner: payable(IS_TEST ? address(this) : msg.sender),
+            owner: payable(_IS_TEST ? address(this) : msg.sender),
             status: true,
             createdAt: block.timestamp
         });
 
-        userProducts[IS_TEST ? address(this) : msg.sender].push(productId);
+        user2products[_IS_TEST ? address(this) : msg.sender][productId] = true;
 
         emit ProductListed(
             productId,
             name,
             price,
-            IS_TEST ? address(this) : msg.sender,
+            _IS_TEST ? address(this) : msg.sender,
             ipfsHash
         );
 
@@ -172,11 +203,11 @@ contract DigitalMarket {
         uint256 ownerAmount = product.price - feeAmount;
 
         // Remove productId from previous owner's list
-        remoteFromUserProducts(product.owner, productId);
+        user2products[product.owner][productId] = false;
 
         // Set new owner and add productId to new owner's list
         product.owner = payable(msg.sender);
-        userProducts[msg.sender].push(productId);
+        user2products[msg.sender][productId] = true;
 
         // Send ETH
         (bool success, ) = previousOwner.call{value: ownerAmount}("");
@@ -214,13 +245,40 @@ contract DigitalMarket {
         address previousOwner = product.owner;
 
         // Remove productId from previous owner's list
-        remoteFromUserProducts(product.owner, productId);
+        user2products[product.owner][productId] = false;
 
         // Set new owner and add productId to new owner's list
         product.owner = payable(newOwner);
-        userProducts[newOwner].push(productId);
+        user2products[newOwner][productId] = true;
 
         emit OwnershipTransferred(productId, previousOwner, newOwner);
+    }
+
+    /*
+     * Withdraw listing fee + transaction fee to platform owner
+     * @params void
+     * @returns void
+     */
+    function withdrawFunds() external onlyPlatformOwner {
+        uint256 balance = address(this).balance;
+        require(balance > 0, "No funds to withdraw");
+
+        (bool success, ) = payable(_platformOwner).call{value: balance}("");
+        require(success, "Withdraw funds failed");
+
+        emit FundsWithdrawn(_platformOwner, balance);
+    }
+
+    /*
+     * Update Platform Owner
+     * @params (address newOwner)
+     * @return void
+     */
+    function updatePlatformOwner(address newOwner) external onlyPlatformOwner {
+        address oldOwner = _platformOwner;
+        _platformOwner = newOwner;
+
+        emit PlatformOwnerChanged(oldOwner, newOwner);
     }
 
     /*
@@ -229,7 +287,7 @@ contract DigitalMarket {
      * @returns void
      */
     function updatePlatformFee(uint256 newFee) external onlyPlatformOwner {
-        require(newFee <= MAX_FEE, "Fee cannot exceed 10%");
+        require(newFee <= MAX_PLATFORM_FEE, "Platform Fee cannot exceed 10%");
 
         uint256 oldFee = platformFee;
         platformFee = newFee;
@@ -238,18 +296,17 @@ contract DigitalMarket {
     }
 
     /*
-     * Withdraw transaction fee
-     * @params void
+     * Update ListingFee
+     * @params (uint256 newFee)
      * @returns void
      */
-    function withdrawFunds() external onlyPlatformOwner {
-        uint256 balance = address(this).balance;
-        require(balance > 0, "No funds to withdraw");
+    function updateListingFee(uint256 newFee) external onlyPlatformOwner {
+        require(newFee <= MAX_LISTING_FEE, "Listing Fee cannot exceed 10%");
 
-        (bool success, ) = payable(platformOwner).call{value: balance}("");
-        require(success, "Withdraw funds failed");
+        uint256 oldFee = listingFee;
+        listingFee = newFee;
 
-        emit FundsWithdrawn(platformOwner, balance);
+        emit ListingFeeUpdated(oldFee, newFee);
     }
 
     /*
@@ -259,15 +316,15 @@ contract DigitalMarket {
      */
     function updateProductStatus(
         uint256 productId,
-        bool enabled
+        bool status
     ) external productExisting(productId) {
         require(
             products[productId].owner == msg.sender,
             "Only owner can do this"
         );
-        products[productId].status = enabled;
+        products[productId].status = status;
 
-        emit ProductAvailabilityChanged(msg.sender, productId, enabled);
+        emit ProductStatusChanged(msg.sender, productId, status);
     }
 
     /*
@@ -282,14 +339,15 @@ contract DigitalMarket {
     }
 
     /*
-     * Get User Product Ids
-     * @params (address user)
-     * @returns (uint256[])
+     * Check if user owns a product
+     * @params (uint256 productId)
+     * @returns bool
      */
-    function getUserProductIds(
-        address user
-    ) external view returns (uint256[] memory) {
-        return userProducts[user];
+    function isUserOwningProduct(
+        uint256 productId
+    ) external view productExisting(productId) returns (bool) {
+        if (products[productId].owner == msg.sender) return true;
+        else return false;
     }
 
     /*
@@ -302,15 +360,20 @@ contract DigitalMarket {
     }
 
     /*
-     * Calculate fee
+     * Calculate platform fee
      * @params (uint256 price)
      * @returns (uint256)
      */
-    function calculateFee(uint256 price) external view returns (uint256) {
+    function calculatePlatformFeePrice(uint256 price) public view returns (uint256) {
         return (price * platformFee) / FEE_DENOMINATOR;
     }
 
-    // FallBacks
-    receive() external payable {}
-    fallback() external payable {}
+    /*
+     * Calculate listing fee
+     * @params (uint256 price)
+     * @returns (uint256)
+     */
+    function calculateListingFeePrice(uint256 price) public view returns (uint256) {
+        return (price * listingFee) / FEE_DENOMINATOR;
+    }
 }
